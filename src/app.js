a7dd824f557e1e0c5a879fcb44f5368559f25c19
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-09-01 16:42:24 +0300

    Add flux method

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Header, UserBlock } from './components';
 import { AppContext } from './context';
 import styles from './app.module.css';
@@ -10,15 +11,59 @@ const getUserFromServer = () => ({
 	phone: '+7-999-999-99-99',
 });
 
+const getNewUserDataFromServer = () => ({
+	id: 'a11101',
+	name: 'Вася',
+	age: 25,
+	email: 'vasya@mail.ru',
+	phone: '+7-999-999-99-98',
+});
+
 export const App = () => {
-	const userData = getUserFromServer();
+	// const userData = getUserFromServer();
+	const [userData, setUserData] = useState(null);
+
+	useEffect(() => {
+		const userDataFromServer = getUserFromServer();
+
+		setUserData(userDataFromServer);
+	}, []);
+
+	const onUserChange = () => {
+		const anotherUserDataFromServer = getNewUserDataFromServer();
+
+		setUserData(anotherUserDataFromServer);
+	};
+
+	const dispatch = (action) => {
+		const { type, payload } = action;
+
+		switch (type) {
+			case 'SET_USER_DATA': {
+				setUserData(payload);
+				break;
+			}
+			case 'SET_USER_AGE': {
+				setUserData({
+					...userData,
+					age: payload,
+				});
+				break;
+			}
+			default:
+			// ничего не делать
+		}
+	};
 
 	return (
-		<AppContext.Provider value={userData}>
+		<AppContext.Provider value={{ userData, dispatch }}>
 			<div className={styles.app}>
 				<Header />
 				<hr />
 				<UserBlock />
+				<button type="button" onClick={onUserChange}>
+					Сменить пользователя
+				</button>
 			</div>
 		</AppContext.Provider>
 	);
--- src/components/user-block/components/user-personal-info/user-personal-info.js
+++ src/components/user-block/components/user-personal-info/user-personal-info.js
@@ -2,13 +2,21 @@ import { useContext } from 'react';
 import { AppContext } from '../../../../context';
 
 export const UserPersonalInfo = () => {
-	const { name, age } = useContext(AppContext);
+	const { userData, dispatch } = useContext(AppContext);
+	const { name, age } = userData;
+
+	const onUserDataUpdate = () => {
+		const newUserData = getNewUserDataFromServer();
+
+		dispatch({ type: 'SET_USER_DATA', payload: newUserData });
+	};
 
 	return (
 		<div>
 			<h3>Персональные данные:</h3>
 			<div>Имя: {name}</div>
 			<div>Возраст: {age}</div>
+			<button onClick={onUserDataUpdate}>Обновить данные пользователя</button>
 		</div>
 	);
 };
